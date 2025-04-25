2ecf1f37d9d9701dd21dcd02d1ba5f461e6f2f7d
Farid3019 <jfflores2005@gmail.com>
2025-04-25 09:41:44 -0600

    Subida de codigo

---
+++ LabProgra1/src/labprogra1/Flores_Jorge_Formulas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogra1;
+
+/**
+ *
+ * @author jfflo
+ */
+public class Flores_Jorge_Formulas {
+    public static void main(String[] args) {
+   
+}
+}
---
+++ LabProgra1/src/labprogra1/Flores_Jorge_PlanillaEmpleado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogra1;
+import java.util.Scanner;
+
+public class Flores_Jorge_PlanillaEmpleado {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String nombre, horasmens, tarifa, salariosem;
+        System.out.println("Porfavor escriba su nombre");
+        nombre = sc.nextLine();
+        System.out.println("Cuantas horas labora al mes?");
+        horasmens = sc.nextLine();
+        System.out.println("De cuanto es su tarifa por hora?");
+        tarifa = sc.nextLine();
+}
+}
