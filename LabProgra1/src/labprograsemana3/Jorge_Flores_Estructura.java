a1404964262ead57c99af26506b553bff2e37e54
Farid3019 <jfflores2005@gmail.com>
2025-05-09 10:39:31 -0600

    se completaron los casos 1 y 2

---
+++ LabProgra1/src/labprograsemana3/Jorge_Flores_Estructura.java
@@ -0,0 +1,71 @@
+
+package labprograsemana3;
+import java.util.Scanner;
+import java.util.Random;
+public class Jorge_Flores_Estructura {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+       
+        System.out.println("Menu");
+        System.out.println("1. Palabras Al Revez");
+        System.out.println("2. Numero Perfecto");
+        System.out.println("3. Primos");
+        System.out.println("4. Votaciones");
+        System.out.println("5. Salir");
+        
+        int menu = sc.nextInt();
+        
+        switch (menu){
+            case 1: 
+                System.out.println("Ingrese su palabra: ");
+                String palabra = sc.next();
+                for (int i = palabra.length() -1;i >=0; i--) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(palabra.charAt(i));
+                    System.out.println("La palabra invertida es: "+sb);
+                    
+                }
+                break;
+            case 2:
+                System.out.println("Ingrese un numero: ");
+                int numero = sc.nextInt();
+                int sumaDiv = 0;
+                for (int i = 1; i < numero; i++) {
+                    if (numero % i ==0){
+                        sumaDiv +=i;
+                    }
+                    
+                }
+                if (sumaDiv == numero){
+                    System.out.println(numero+ " es un numero perfecto");
+                }
+                else{
+                    System.out.println(numero+" no es un numero perfecto");
+                }
+                break;
+            case 3:
+               Random ran = new Random();
+             int num = ran.nextInt(100)+1;
+             int contador = 0;
+             StringBuilder sbdivisores = new StringBuilder();
+                for (int i = 1;i < num; i++) {
+                    if (num% i == 0){
+                        contador++;
+                        sbdivisores.append(i);
+                    }
+                    if (i < num){
+                        sbdivisores.append(",");
+                    }
+                    
+                }
+                if (contador ==2){
+                    System.out.println(num+" es primo porque solo tiene 2 divisores");
+                }
+                else{
+                    System.out.println(num+ " no es primo porque tiene "+contador+" divisores");
+                }
+                System.out.println("Divisores de "+num+" son "+sbdivisores);
+        }
+        }    
+    }
+  
